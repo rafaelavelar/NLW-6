d4d657245fff82295da1fc678cd49090b9db3181
rafaelavelar <ra.fa89@hotmail.com>
2023-02-09 15:54:37 -0300

    estrutura e estilização dos depoimentos, biblioteca swiper para slides, biblioteca scrollreveal e estrutura e estilização do contato

--- Origin/script.js
+++ Origin/script.js
@@ -28,3 +28,32 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scroll");
   }
 });
+
+// Testimonial carousel
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+// ScrollReveal
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testmonials,
+  #contact .text, #contact .links  
+`,
+  { interval: 100 }
+);
