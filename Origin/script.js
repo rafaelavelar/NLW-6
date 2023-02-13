3fa659578a3dad8dac6ac90977ff4042fdb48bc5
rafaelavelar <ra.fa89@hotmail.com>
2023-02-13 13:59:21 -0300

    responsividade e update do javascript

--- Origin/script.js
+++ Origin/script.js
@@ -1,7 +1,6 @@
 // abre e fecha o menu quando clicar no icone
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
-
 for (const element of toggle) {
   element.addEventListener("click", function () {
     nav.classList.toggle("show");
@@ -10,7 +9,6 @@ for (const element of toggle) {
 
 // quando clicar em um item, fechar o menu
 const links = document.querySelectorAll("nav ul li a");
-
 for (const link of links) {
   link.addEventListener("click", function () {
     nav.classList.remove("show");
@@ -20,7 +18,6 @@ for (const link of links) {
 // add sombra no header
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
-
 function changeHeader() {
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
@@ -37,6 +34,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 // ScrollReveal
@@ -66,6 +69,31 @@ function backToTop() {
     backButton.classList.add("show");
   } else {
     backButton.classList.remove("show");
+  }
+}
+
+// Menu ativo conforme seção visivel na página
+const sections = document.querySelectorAll("main section[id]");
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
   }
 }
 
